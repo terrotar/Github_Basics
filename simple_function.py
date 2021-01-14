7e64f75906d9387e8d259090e22551d9c588b762
terrotar <pedro_guedes93@hotmail.com>
2021-01-13 23:17:10 -0300

    Arquivo simple_function.py alterado.

--- simple_function.py
+++ simple_function.py
@@ -1,25 +1,41 @@
 
-a = int(input("Type the first number: "))
-b = int(input("Type the second number: "))
+try:
+    a = float(input("Type the first number: "))
+    b = float(input("Type the second number: "))
 
-
-def operation(a, b):
-    op = int(input(f"""
+    def operation(a, b):
+        op = int(input(f"""
+        {50 * "*"}\n
+        Type the kind of operation you desire to do:\n
+        Addition ----- 1\n
+        Subtraction ----- 2\n
+        Multiplication ----- 3\n
+        Division ----- 4\n
+        """))
+        if (op == 1):
+            return (f"Addition of {a} and {b} is: {a+b}.")
+        if (op == 2):
+            return (f"Subtraction of {a} and {b} is: {a-b}.")
+        if (op == 3):
+            return (f"Multiplication of {a} and {b} is: {a*b}.")
+        if (op == 4):
+            return (f"Division of {a} and {b} is: {a/b}.")
+    print(operation(a, b))
+except ValueError:
+    print(f"""
     {50 * "*"}\n
-    Type the kind of operation you desire to do:\n
-    Addition ----- 1\n
-    Subtraction ----- 2\n
-    Multiplication ----- 3\n
-    Division ----- 4\n
-    """))
-    if (op == 1):
-        return (f"Addition of {a} and {b} is: {a+b}.")
-    if (op == 2):
-        return (f"Subtraction of {a} and {b} is: {a-b}.")
-    if (op == 3):
-        return (f"Multiplication of {a} and {b} is: {a*b}.")
-    if (op == 4):
-        return (f"Division of {a} and {b} is: {a/b}.")
-
-
-print(operation(a, b))
+    Sorry, the character typed is not a number.\n
+    Please enter a valid number.
+    """)
+except ZeroDivisionError:
+    print(f"""
+    {50 * "*"}\n
+    Sorry, you can't divide a number by 0.\n
+    Please enter a valid number.
+    """)
+except AssertionError:
+    print(f"""
+{50 * "*"}\n
+    Sorry, an unexpected error occurred.\n
+    Please, try again...
+    """)
